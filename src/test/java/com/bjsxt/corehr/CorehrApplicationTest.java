4b5e387c14b6b3ff4fe8a7461f9939c03da2a779
rendeyou <280732160@qq.com>
2022-03-12 20:53:27 +0800

    测试的多线程

---
+++ src/test/java/com/bjsxt/corehr/CorehrApplicationTest.java
@@ -0,0 +1,21 @@
+package com.bjsxt.corehr;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @className: CorehrApplicationTest
+ * @description: // TODO
+ * @author: RenDeyou
+ * @date: 2022/3/12 20:50
+ */
+
+@Slf4j
+@SpringBootTest
+class CorehrApplicationTest {
+
+    public static void main(String[] args) {
+        log.info("启动类的测试类");
+    }
+
+}
